0e2ade929825bf290d94c703133ffd4ac70b8f28
Matt Rathburn <m.rathburn@ymail.com>
2015-11-15 17:45:50 -0600

    bbbbbbb

--- app/src/main/java/project215/project215/Login.java
+++ app/src/main/java/project215/project215/Login.java
@@ -10,13 +10,35 @@ Still needs implementation.
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    android.widget.Button button1;
+    android.widget.Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Goto Map View
+
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Goto Register View
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/project215/project215/Register.java
+++ app/src/main/java/project215/project215/Register.java
@@ -13,27 +13,31 @@ import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
-    //get the email and password still
-
     android.widget.Button button1;
     android.widget.Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        button1 = (Button) findViewById(R.id.button);
+        button1 = (Button) findViewById(R.id.button3);
         button1.setOnClickListener(new OnClickListener() {
+            @Override
             public void onClick(View v) {
-                //Goto Map View
+
+                //register and go back to the login screen to login
+
             }
         });
 
-        button2 = (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button4);
         button2.setOnClickListener(new OnClickListener() {
+            @Override
             public void onClick(View v) {
-                //Goto Register View
+                //cancel registration
             }
         });
+
     }
 }
