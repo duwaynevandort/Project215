6e489362606cef558a514e56181c7576c38f4b16
Adaminius <adam.mansfield@yahoo.com>
2015-11-16 15:17:22 -0600

    Implement UserModel.checkUser()
    Not tested

--- app/src/main/java/project215/project215/UserModel.java
+++ app/src/main/java/project215/project215/UserModel.java
@@ -6,8 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 
 /**
  * Created by Adam on 11/15/2015.
@@ -67,16 +66,62 @@ public class UserModel
                 catch (IOException e) {
                     e.printStackTrace();
                 }
-                catch (JSONException e) {
-                    e.printStackTrace();
-                }
 
+        // TODO: 11/16/2015 test this 
         return success;
     }
 
-    public static int getUserByEmail(String email)
+    public static boolean checkUser(String username, String passwordHash)
     {
-        // TODO: 11/15/2015
-        return 0;
+        boolean success = false;
+
+        JSONObject json = new JSONObject();
+        URL url;
+        HttpURLConnection urlConnection;
+        DataOutputStream printout;
+
+        try{
+            //url for login
+            String http = "http://162.243.52.70:7000/user/login/";
+
+            //make url object
+            url = new URL (http);
+            //open the connection, set methods and stuff
+            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setRequestProperty("Content-Type","application/json");
+
+            //Creating the JSON object, it's basically a hash map
+            //json.put(KEY, VALUE)
+            json.put("username", username);
+            json.put("password", passwordHash);
+
+            printout = new DataOutputStream(urlConnection.getOutputStream ());
+
+            String str = json.toString();
+            byte[] data=str.getBytes("UTF-8");
+            printout.write(data);
+            printout.flush ();
+            printout.close ();
+
+            if(urlConnection.getResponseCode() == SUCCESS_CODE)
+                success = true;
+
+            System.out.print("Json String:   " + json.toString());
+            System.out.print("\nResponse code: " + urlConnection.getResponseCode());
+
+            urlConnection.disconnect();
+
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: 11/16/2015 test this 
+        return success;
     }
 }
