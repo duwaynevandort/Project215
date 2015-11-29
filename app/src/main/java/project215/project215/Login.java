9aa24cb2937bb5b0d156600f0eab754e02b9f591
KennyCook <krc032@shsu.edu>
2015-11-28 18:53:51 -0600

    App mostly functional, just needs final touches (11/28/15)

---
+++ app/src/main/java/project215/project215/Login.java
@@ -0,0 +1,50 @@
+package project215.project215;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    private EditText emailField;
+    private EditText passwordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailField = (EditText) findViewById(R.id.login_screen_email_entry);
+        passwordField = (EditText)findViewById(R.id.login_screen_password_entry);
+    }
+
+    public void submitLogin(View view)
+    {
+        String emailEntered = emailField.getText().toString();
+        String passwordEntered = passwordField.getText().toString();
+
+        //TODO if login failed, handle the failure appropriately
+
+        if( SuperController.checkUser(emailEntered, passwordEntered) )
+        {
+            Intent loginSuccess = new Intent(this, Test.class);
+            startActivity(loginSuccess);
+        }
+        else
+        {
+            Toast.makeText(Login.this, "Login failed. If you just registered, please check your" +
+                    "email for an activation link.", Toast.LENGTH_SHORT).show();
+            emailField.setText("");
+            passwordField.setText("");
+        }
+    }
+
+    public void switchToRegisterScreen(View view)
+    {
+        Intent register = new Intent(this, Register.class);
+        startActivity(register);
+    }
+}
