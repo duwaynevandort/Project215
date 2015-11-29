361d628b8b5c17b2281eac24016768eb433a361c
KennyCook <krc032@shsu.edu>
2015-11-28 18:48:31 -0600

    App mostly functional, just needs final touches (11/28/15)

--- app/src/main/java/project215/project215/SuperController.java
+++ app/src/main/java/project215/project215/SuperController.java
@@ -5,74 +5,223 @@ import android.util.Log;
 
 import java.util.List;
 
+/*
+*   Author: Kenny Cook
+*
+*   This class acts as a mediator between the views (activities)
+*       and the models. It is accessible by any activity in the
+*       application
+*/
+
 public class SuperController extends Application
 {
-    public static int userID;
+    //set to negative after testing?
+    private static int userID = 112;
+
+    private static boolean pinSubmitted;
+    private static boolean voteSubmitted;
+    private static boolean reportSubmitted;
+    private static boolean userCreated;
+    private static boolean validUser;
+    private static List<Pin> pinList;
 
     private static final String TAG = "Controller Log";
 
     /**************************************PIN CONTROLLER****************************************/
 
-    public void createPin(double latitude, double longitude, String category, String description)
+    public static boolean createPin(final double latitude, final double longitude, final String category, final String description)
     {
-        if ( PinModel.createPinRecord(latitude, longitude, category, description, userID) )
-        {
-            Log.d(TAG, "Pin created successfully");
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    if ( PinModel.createPinRecord(latitude, longitude, category, description, userID) )
+                    {
+                        Log.d(TAG, "Pin created successfully");
+                        pinSubmitted = true;
+                    }
+                    else
+                    {
+                        Log.d(TAG, "Pin creation failed");
+                        pinSubmitted = false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        else
-        {
-            Log.d(TAG, "Pin creation failed");
+
+        return pinSubmitted;
+    }
+
+    public void setVote(final int pinID, final boolean isHere)
+    {
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    if (VoteModel.createVoteRecord(userID, pinID, isHere)) {
+                        Log.d(TAG, "Vote logged successfully");
+                    } else {
+                        Log.d(TAG, "Failed to log vote");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
-    public void setVote(int pinID, boolean isHere)
+    public void setReport(final int pinID)
     {
-        if (VoteModel.createVoteRecord(userID, pinID, isHere))
-        {
-            Log.d(TAG, "Vote logged successfully");
-        }
-        else
-        {
-            Log.d(TAG, "Failed to log vote");
-        }
-    }
-
-    public void setReport(int pinID)
-    {
-        if ( ReportModel.createReportRecord(userID, pinID) )
-        {
-            Log.d(TAG, "Report logged successfully");
-        }
-        else
-        {
-            Log.d(TAG, "Failed to log report");
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    if ( ReportModel.createReportRecord(userID, pinID) )
+                    {
+                        Log.d(TAG, "Report logged successfully");
+                    }
+                    else
+                    {
+                        Log.d(TAG, "Failed to log report");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
     /**************************************USER CONTROLLER****************************************/
 
-    //userID = return values for both?
-
-    public boolean checkUser(String userEmail, String userPassword)
+    public static boolean checkUser(final String userEmail, String userPassword)
     {
-        String hashedPassword = "butts";
-        //Bcrypt stuff
+        final String hashedPassword = userPassword;
 
-        return UserModel.checkUser(userEmail, hashedPassword);
+        //TODO implement initial encryption for password string
+
+        //TODO have Adam change getUserID to bool type, implement, test
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    if ( UserModel.checkUser(userEmail, hashedPassword) )
+                    {
+                        validUser = true;
+                        Log.d(TAG, "User verification success!" + validUser);
+                    }
+                    else
+                    {
+                        validUser = false;
+                        Log.d(TAG, "User verification failed!" + validUser);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "validUser = " + validUser);
+        return validUser;
     }
 
-    public boolean createUser(String userEmail, String userPassword)
+    public static boolean createUser(final String userEmail, String userPassword)
     {
-        String hashedPassword = "butts";
-        //Bcrypt stuff
+        final String hashedPassword = userPassword;
 
-        return UserModel.createUser(userEmail, hashedPassword);
+        //TODO implement initial encryption for password string
+
+        //TODO have Adam change getUserID to bool type, implement, test
+
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    if (UserModel.createUser(userEmail, hashedPassword) )
+                    {
+                        userCreated = true;
+                        Log.d(TAG, "User created successfully" + userCreated);
+                    }
+                    else
+                    {
+                        userCreated = false;
+                        Log.d(TAG, "User creation failed" + userCreated);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "userCreated = " + userCreated);
+        return userCreated;
     }
 
     /****************************************MAP CONTROLLER***************************************/
 
-    public List<Pin> getPins(double latitude1, double longitude1, double latitude2, double longitude2)
+    public List<Pin> getPinList(final double latitude1, final double longitude1, final double latitude2, final double longitude2)
     {
-        return PinModel.getPinByBounds(latitude1, longitude1, latitude2, longitude2);
+        this.getPins(-1000, -1000, 1000, 1000);
+        return pinList;
+    }
+
+
+    private void getPins(final double latitude1, final double longitude1, final double latitude2, final double longitude2)
+    {
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    pinList = PinModel.getPinByBounds(latitude1, longitude1, latitude2, longitude2);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
